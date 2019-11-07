0ba1b4af9b1dcdc99c4843394cf45a5c722b005c
luisthmn <luisernestothomson@hotmail.com>
2019-11-06 23:23:10 -0700

    zas

--- tokenizer/tokenizer/bloques.h
+++ tokenizer/tokenizer/bloques.h
@@ -10,19 +10,17 @@ enum{IF, WHILE};
 
 struct bloque {
 	string tipo;
-
-<<<<<<< HEAD
 	int linInicio;
-	int linFinal;
+	int linFin;
 	int length;
 
 	bloque *anterior;
 	bloque *siguiente;
 };
-struct simbolo{
 
+struct simbolo{
 	string tipo;
-	bloque *anterior;
-=======
->>>>>>> 395f0838c7ef5bb043017c9b0d7119d3ac28d35e
+	int lin;
+
+	simbolo *anterior;
 };
